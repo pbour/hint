715fb9b39c6ec1d823daa56df5e174a318567143
Panagiotis Bouros <12070531+pbour@users.noreply.github.com>
2022-11-07 11:17:19 +0100

    Improvements on comparison-free HINT

--- containers/offsets_templates.h
+++ containers/offsets_templates.h
@@ -48,6 +48,10 @@ public:
     ~OffsetEntry_SS();
 };
 
+// For HINT
+typedef OffsetEntry_SS<RelationId>    OffsetEntry_SS_HINT;
+
+// For HINT^m
 typedef OffsetEntry_SS<Relation>      OffsetEntry_SS_OrgsIn;
 typedef OffsetEntry_SS<RelationStart> OffsetEntry_SS_OrgsAft;
 typedef OffsetEntry_SS<RelationEnd>   OffsetEntry_SS_RepsIn;
@@ -63,11 +67,15 @@ public:
     ~Offsets_SS();
 };
 
+// For HINT
+typedef Offsets_SS<RelationId> Offsets_SS_HINT;
+typedef Offsets_SS_HINT::const_iterator Offsets_SS_HINT_Iterator;
+
+// For HINT^m
 typedef Offsets_SS<Relation>      Offsets_SS_OrgsIn;
 typedef Offsets_SS<RelationStart> Offsets_SS_OrgsAft;
 typedef Offsets_SS<RelationEnd>   Offsets_SS_RepsIn;
 typedef Offsets_SS<RelationId>    Offsets_SS_RepsAft;
-
 typedef Offsets_SS_OrgsIn::const_iterator  Offsets_SS_OrgsIn_Iterator;
 typedef Offsets_SS_OrgsAft::const_iterator Offsets_SS_OrgsAft_Iterator;
 typedef Offsets_SS_RepsIn::const_iterator  Offsets_SS_RepsIn_Iterator;
